eb2418053fd32067fccd16fb6ec7c1eafb6e28fd
Lorenzo Venturini <102552226+LorenzoVA@users.noreply.github.com>
2022-04-25 12:00:37 -0300

    Delete MariaDB directory

--- MariaDB/1. Script do Usuário e do Banco de Dados.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER lorenzo;
-
-CREATE DATABASE `uvv` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-GRANT ALL PRIVILEGES ON uvv.* TO lorenzo;
--- MariaDB/README.md
+++
@@ -1,2 +0,0 @@
-# MariaDB
-Aqui estão os scripts do banco de dados do MariaDB, eles estão enumerados na ordem de execução
