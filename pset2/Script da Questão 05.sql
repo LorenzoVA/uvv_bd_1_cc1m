f0e0cb86341c062ff20d5fb5ec30097a5b947b28
Lorenzo Venturini <102552226+LorenzoVA@users.noreply.github.com>
2022-05-10 16:14:37 -0300

    Update Script da Questão 05.sql

--- pset2/Script da Questão 05.sql
+++ pset2/Script da Questão 05.sql
@@ -4,4 +4,4 @@ case when d.cpf_gerente != f.cpf then primeiro_nome end funcionario, concat (f.s
 from elmasri.funcionario f
 inner join elmasri.departamento d
 on f.numero_departamento = d.numero_departamento
-order by d.nome_departamento asc, f.salario;
+order by d.nome_departamento asc, f.salario desc;
