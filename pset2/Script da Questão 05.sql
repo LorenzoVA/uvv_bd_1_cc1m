619a55540dcab9005aa595cc92c17b8c3eb39cb5
Lorenzo Venturini <102552226+LorenzoVA@users.noreply.github.com>
2022-05-10 15:47:41 -0300

    Update Script da Questão 05.sql

--- pset2/Script da Questão 05.sql
+++ pset2/Script da Questão 05.sql
@@ -1,8 +1,6 @@
 select d.nome_departamento departamento,
-case when d.cpf_gerente = f.cpf then primeiro_nome
-end gerente,
-case when d.cpf_gerente != f.cpf then primeiro_nome
-end funcionario, concat (f.salario) salario
+case when d.cpf_gerente = f.cpf then primeiro_nome end gerente,
+case when d.cpf_gerente != f.cpf then primeiro_nome end funcionario, concat (f.salario) salario
 from elmasri.funcionario f
 inner join elmasri.departamento d
 on f.numero_departamento = d.numero_departamento
