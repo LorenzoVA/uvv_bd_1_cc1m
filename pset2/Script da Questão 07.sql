fdfb2bff2bbffe50b099b25cb8be646d9792e200
Lorenzo Venturini <102552226+LorenzoVA@users.noreply.github.com>
2022-05-10 16:00:44 -0300

    Update Script da Questão 07.sql

--- pset2/Script da Questão 07.sql
+++ pset2/Script da Questão 07.sql
@@ -1,3 +1,3 @@
-select concat (primeiro_nome, nome_meio, ultimo_nome) as nome_funcionario, numero_departamento as departamento, salario
+select concat (primeiro_nome, nome_meio, ultimo_nome) nome_funcionario, numero_departamento departamento, salario
 from elmasri.funcionario
 where exists (select * from elmasri.dependente where funcionario.cpf != dependente.cpf_funcionario);
