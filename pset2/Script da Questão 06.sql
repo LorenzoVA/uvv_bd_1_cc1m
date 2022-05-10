dbce5bf0ebac7075fdb3355e372a3e33ec77a284
Lorenzo Venturini <102552226+LorenzoVA@users.noreply.github.com>
2022-05-10 16:06:27 -0300

    Update Script da Questão 06.sql

--- pset2/Script da Questão 06.sql
+++ pset2/Script da Questão 06.sql
@@ -1,4 +1,4 @@
-select concat(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) funcionario, f.cpf, numero_departamento, concat(d.nome_dependente, ' ', f.ultimo_nome) nome_dependente, age(current_date,d.data_nascimento) idade_dependente, 
+select concat(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) nome_funcionario, f.cpf, numero_departamento, concat(d.nome_dependente, ' ', f.ultimo_nome) nome_dependente, age(current_date,d.data_nascimento) idade_dependente, 
 case d.sexo when 'm' then 'masculino' when 'f' then 'feminino' end sexo
 from elmasri.funcionario f
 inner join elmasri.dependente d
